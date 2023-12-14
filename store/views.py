eb0aa798215cb56f31f3a82fee5352741fb84a3f
Álvaro Manterola <alvaro.manterola@gmail.com>
2023-12-14 04:22:18 -0300

    feature: added front to home page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,23 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import ListView
+from .models import Customer
+
+User = get_user_model()
 
 
-def index(request):
-    return render(request, "index.html")
+class IndexView(View):
+    def get(self, request):
+        context = {}
+        try:
+            user = User.objects.get(id=request.user.id)
+            context["user"] = user
+        except User.DoesNotExist:
+            user = None
+        try:
+            customer = Customer.objects.get(id_user_id=user)
+            context["customer"] = customer
+        except Customer.DoesNotExist:
+            pass
+        return render(request, "index.html", context)
