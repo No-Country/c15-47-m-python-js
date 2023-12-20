bb04bcd3180b4cf7a6769ff1e003c84c006c010f
GustavoJ <122030235+gjaguirre@users.noreply.github.com>
2023-12-19 22:08:47 -0300

    add comments in admin.

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Author, Book, Category, Customer, Publisher 
 
-# Register your models here.
+# Register your models here. To verify that all the models are registered
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(Category)
